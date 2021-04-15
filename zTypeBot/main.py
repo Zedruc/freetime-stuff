8e6ed70b9533fd220b10fca82d9ad1d8530c5ca2
Nofu <69540929+Zedruc@users.noreply.github.com>
2021-04-15 18:39:38 +0200

    o h b o y

--- zTypeBot/main.py
+++ zTypeBot/main.py
@@ -1,10 +1,16 @@
 import webbrowser  # To open ztype
 import pyscreenshot as ImageGrab
+from PIL import Image
+import os
 import cv2
 import time
+import numpy as np
 import pytesseract
 from pynput.keyboard import Key, Controller
 from pynput.mouse import Button, Controller as MController
+
+frame = 0
+
 keyboard = Controller()
 mouse = MController()
 webbrowser.open_new("https://zty.pe/")
@@ -17,13 +23,16 @@ time.sleep(2)
 mouse.press(Button.left)  # To make 100% sure the browser window is focused
 mouse.release(Button.left)
 
-pytesseract.pytesseract.tesseract_cmd = r'c:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+dir_path = os.getcwd()
+pytesseract.pytesseract.tesseract_cmd = r'{0}\tesseract\Tesseract-OCR\tesseract.exe'.format(
+    dir_path)
 
 
 def screen():
     # upper half of the game
-    im = ImageGrab.grab(bbox=(740, 200, 1210, 700))
+    im = ImageGrab.grab(bbox=(720, 200, 1210, 840))
     im.save("data/img.png")
+    im.save(f"{os.getcwd()}\\data\\detections_frames\\frame{frame}.png")
     img = cv2.imread('data/img.png')
     text = pytesseract.image_to_string(img)
     # make it an array so it can type stuff letter by letter ( Just very fast B) )
@@ -32,12 +41,9 @@ def screen():
     if(len(text_list) < 1):
         return
     for character in text_list:
-        if(character == "\\n"):
-            continue
-        if(character == "\\x0c"):
-            continue
-        if(character == ", "):
-            continue
+        text_list = text_list[:len(text_list)-2]
+        if(", " in text_list):
+            text_list.remove(", ")
 
         print(character)
         if(character.isalpha()):
@@ -50,4 +56,4 @@ def screen():
 
 while True:
     screen()
-    time.sleep(1)
+    frame += 1
