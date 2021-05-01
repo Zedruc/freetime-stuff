9c9e9060cddbdc6b256ba7f35bfdcf840f39f736
Nofu <69540929+Zedruc@users.noreply.github.com>
2021-05-01 20:24:15 +0200

    Added examples to show the different ways of using the library

---
+++ graph.js/examples/basic/main.js
@@ -0,0 +1,32 @@
+import GraphJS from "../../src/index.js";
+var data = [
+    [100, 200],
+    [200, 300],
+    [700, 400],
+    [800, 500],
+    [1500, 200]
+]
+
+var width = 5;
+
+// Initialize GraphJS with the canvas to use as graph
+GraphJS.init(document.getElementById('graph'));
+
+// Set the data to draw (x, y pairs called coordination sets)
+GraphJS.setData(data);
+
+
+//#region  Optional styling
+GraphJS.setLineColor("Blizzard Blue") // Defaults to black without a parameter
+
+// setLineColor accepts HEX and names of predefined colors
+// To list them call .colorList()
+GraphJS.colorList(); // Accepts HTMLElement as parameter to show the colors in it
+
+// Sets the width of the drawn lines
+GraphJS.setLineWidth(width);
+//#endregion
+
+
+// Finally, draw the styled and set data to the graph
+GraphJS.drawGraph();
---
+++ graph.js/examples/config from json/main.js
@@ -0,0 +1,7 @@
+import GraphJS from "../../src/index.js";
+
+// Load data from config.json
+GraphJS.loadFromConfigJSON("./config.json");
+
+// Draw the graph
+GraphJS.drawGraph();
