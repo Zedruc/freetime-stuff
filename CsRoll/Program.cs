863c0583e5e41dcb77ac4329a40fc8d8cae6952c
Nofu <69540929+Zedruc@users.noreply.github.com>
2022-10-15 22:48:09 +0200

    Summary :fax:

--- CsRoll/Program.cs
+++ CsRoll/Program.cs
@@ -1,23 +1,21 @@
 ﻿using System;
 
-namespace CsRoll
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var _ =
+            "68747470733a2f2f7777772e796f75747562652e636f6d2f77617463683f763d7876465a6a6f3550674730";
+        byte[] raw = new byte[_.Length / 2];
+        for (int i = 0; i < raw.Length; i++)
         {
-            var _ =
-                "68747470733a2f2f7777772e796f75747562652e636f6d2f77617463683f763d7876465a6a6f3550674730";
-            byte[] raw = new byte[_.Length / 2];
-            for (int i = 0; i < raw.Length; i++)
-            {
-                raw[i] = Convert.ToByte(_.Substring(i * 2, 2), 16);
-            }
-            var __ = System.Text.Encoding.UTF8.GetString(raw);
-            var ___ = new System.Diagnostics.ProcessStartInfo();
-            ___.UseShellExecute = true;
-            ___.FileName = __;
-            System.Diagnostics.Process.Start(___);
+            raw[i] = Convert.ToByte(_.Substring(i * 2, 2), 16);
         }
+        var __ = System.Text.Encoding.UTF8.GetString(raw);
+        var ___ = new System.Diagnostics.ProcessStartInfo();
+        ___.UseShellExecute = true;
+        ___.FileName = __;
+        System.Diagnostics.Process.Start(___);
+        System.Threading.Thread.Sleep(500);
     }
 }
